227188dc73e1542bfb99818eda3c1c6dd37f7f1d
Melksedeque <freelancer@melksedeque.com.br>
2025-05-17 16:14:14 -0300

    Criação de página administrativa de Restaurantes

--- src/paginas/Administracao/Restaurantes/index.tsx
+++ src/paginas/Administracao/Restaurantes/index.tsx
@@ -1,5 +1,39 @@
+import { useEffect, useState } from "react";
+import IRestaurante from "../../../interfaces/IRestaurante";
+import axios from "axios";
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+
 const AdministracaoRestaurantes = () => {
-    return(<h1>Admin Restaurantes</h1>);
+    const [restaurantes, steRestaurantes] = useState<IRestaurante[]>([]);
+
+    useEffect(() => {
+        axios.get<IRestaurante[]>('http://localhost:8000/api/v2/restaurantes/')
+        .then(resposta => {
+            steRestaurantes(resposta.data)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+    }, [])
+
+    return(
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Nome</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map((restaurante) => (
+                        <TableRow key={restaurante.id}>
+                            <TableCell>{restaurante.nome}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
 }
 
 export default AdministracaoRestaurantes;
