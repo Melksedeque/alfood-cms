175bb9b5fbe449660e8f28739f10f95077f43c8e
Melksedeque <freelancer@melksedeque.com.br>
2025-05-18 13:57:10 -0300

    Mudança no tamanho do Container

--- src/paginas/Administracao/AdminApp/index.tsx
+++ src/paginas/Administracao/AdminApp/index.tsx
@@ -1,14 +1,15 @@
 import { Box, Container, Paper } from "@mui/material"
 import AppHeader from "./AppHeader";
+import { Outlet } from "react-router-dom";
 
 const AdminApp = () => {
     return (
         <>
             <AppHeader />
             <Box>
-                <Container maxWidth="lg">
+                <Container maxWidth="xl">
                     <Paper sx={{p: 3}}>
-                        {/* Conteúdo da Página Admin */}
+                        <Outlet />
                     </Paper>
                 </Container>
             </Box>
