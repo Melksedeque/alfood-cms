18b36bbd75bf4b016fff6c823adc69a804946480
Melksedeque <freelancer@melksedeque.com.br>
2025-05-18 14:24:36 -0300

    Formulário de cadastro e edição de pratos

---
+++ src/paginas/Administracao/Pratos/form.tsx
@@ -0,0 +1,63 @@
+import { Box, Button, TextField, Typography, Container } from "@mui/material"
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import http from "../../../http";
+import IPrato from "../../../interfaces/IPrato";
+
+const FormularioPrato = () => {
+    const params = useParams();
+    useEffect(() => {
+        if (params.id) {
+            http.get<IPrato>(`/pratos/${params.id}/`)
+                .then(resposta => setNomePrato(resposta.data.nome))
+        }
+    }, [params])
+
+    const [nomePrato, setNomePrato] = useState<string>('');
+    const aoSubmeterForm = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        if (params.id) {
+            http.put<IPrato>(`/restaurantes/${params.id}/`, { nome: nomePrato })
+               .then(() => {
+                    alert('Restaurante atualizado com sucesso!')
+                })
+            return;
+        } else {
+            http.post('/restaurantes/', { nome: nomePrato })
+            .then(() => {
+                alert('Restaurante cadastrado com sucesso!')
+            })
+        }
+    }
+
+    return (
+        <Container maxWidth="lg">
+            <Box sx={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+                <Typography component="h1" variant="h4">
+                    {params.id? 'Editar' : 'Cadastrar'} Prato
+                </Typography>
+                <Box component="form" onSubmit={aoSubmeterForm} sx={{width: '100%'}}>
+                    <TextField
+                        id="standard-basic"
+                        label="Nome do Prato"
+                        variant="standard"
+                        value={nomePrato}
+                        onChange={event => setNomePrato(event.target.value)}
+                        fullWidth
+                        required
+                    />
+                    <Button
+                        type="submit"
+                        variant="outlined"
+                        fullWidth
+                        sx={{marginTop: 2}}
+                    >
+                        Salvar
+                    </Button>
+                </Box>
+            </Box>
+        </Container>
+    )
+}
+
+export default FormularioPrato;
