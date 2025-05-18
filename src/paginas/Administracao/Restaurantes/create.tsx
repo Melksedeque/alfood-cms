2c5b37145c6c5d7fd14d7e85606637c7262b8c4c
Melksedeque <freelancer@melksedeque.com.br>
2025-05-18 12:21:13 -0300

    useEffect para editar restaurante

--- src/paginas/Administracao/Restaurantes/create.tsx
+++ src/paginas/Administracao/Restaurantes/create.tsx
@@ -1,8 +1,19 @@
 import { Button, TextField } from "@mui/material"
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import IRestaurante from "../../../interfaces/IRestaurante";
 
 const FormularioRestaurante = () => {
+    const params = useParams();
+
+    useEffect(() => {
+        if (params.id) {
+            axios.get<IRestaurante>(`http://localhost:8000/api/v2/restaurantes/${params.id}`)
+                .then(resposta => setNomeRestaurante(resposta.data.nome))
+        }
+    }, [params])
+
     const [nomeRestaurante, setNomeRestaurante] = useState<string>('');
     const aoSubmeterForm = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
