2db67669389ee20dd405697d6b4402474ca12b7a
Melksedeque <freelancer@melksedeque.com.br>
2025-05-19 16:00:38 -0300

    Listar pratos de cada restaurante n página de Restaurantes no site

--- src/componentes/ListaRestaurantes/Restaurante/index.tsx
+++ src/componentes/ListaRestaurantes/Restaurante/index.tsx
@@ -1,21 +1,34 @@
+import { useEffect, useState } from 'react';
 import IRestaurante from '../../../interfaces/IRestaurante';
 import Prato from '../Prato';
 import estilos from './Restaurante.module.scss';
+import IPrato from '../../../interfaces/IPrato';
+import axios from 'axios';
 
 interface RestauranteProps {
   restaurante: IRestaurante
 }
 
 const Restaurante = ({ restaurante }: RestauranteProps) => {
+  const [pratos, setPratos] = useState<IPrato[]>([]);
 
-  console.log(restaurante.pratos)
+  useEffect(() => {
+    document.title = 'Pratos'
+    axios.get<IPrato[]>(`http://localhost:8000/api/v1/restaurantes/${restaurante.id}/pratos/`)
+      .then(resposta => {
+        setPratos(resposta.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [restaurante.id])
 
   return (<section className={estilos.Restaurante}>
     <div className={estilos.Titulo}>
       <h2>{restaurante.nome}</h2>
     </div>
     <div>
-      {restaurante.pratos?.map(item => <Prato prato={item} key={item.id} />)}
+      {pratos?.map(item => <Prato prato={item} key={item.id} />)}
     </div>
   </section>)
 }
